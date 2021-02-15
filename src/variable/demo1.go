67481f3716991336a8cb31ff5e21f55cc9543365
jackbaul <22878236@qq.com>
2021-02-15 12:11:20 +0800

    variable reclare

--- src/variable/demo1.go
+++ src/variable/demo1.go
@@ -1,12 +1,25 @@
 package main
 
 import "fmt"
+
 //设置全局变量
 var (
-	x=100
-	y="good"
-	z=80.90
+	x = 100
+	y = "good"
+	z = 80.90
 )
-func main(){
-	fmt.Println("x=",x,"y=",y,"z=",z)
+var block="alibaba"
+func main() {
+	var x = 200
+	fmt.Println("x=", x, "y=", y, "z=", z)
+
+	//以下演示变量重名
+	block := "function"
+	{
+		block := "inner"
+		fmt.Printf("The block is %s.\n", block)
+	}
+	fmt.Printf("The block is %s.\n", block)
+
+
 }
